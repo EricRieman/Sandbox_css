1997f0adf704e87c27545139a74e4821e365ca2a
Eric Rieman <er28@zips.uakron.edu>
2021-06-20 17:27:26 -0400

    Setup prjoect to learn bootstrap

---
+++ learn-bootstrap/src/App.js
@@ -0,0 +1,11 @@
+import "./style.css"
+
+function App() {
+  return (
+    <div>
+      Hello
+    </div>
+  );
+}
+
+export default App;
---
+++ learn-bootstrap/src/index.js
@@ -0,0 +1,13 @@
+// Tutorial repo can be found here:
+// https://github.com/Video-Lab/css-course-content
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
